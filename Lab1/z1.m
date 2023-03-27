f5e6a1967662f47cab9956fd3410f477be46aba8
eSqadron <kubam0906@gmail.com>
2023-03-27 21:20:52 +0200

    badanie

--- Lab1/z1.m
+++ Lab1/z1.m
@@ -1,46 +1,46 @@
 f1 = 1000;
 f2 = [1003, 1006, 1009, 1012, 1015, 1018];
 
-correctness = zeros(1, 6);
-un_correctness = zeros(1, 6);
+correctness = zeros(1, length(f2));
+un_correctness = zeros(1, length(f2));
 
 fs = 44100;
 
 test = generate_sine(fs, f1);
 
 
-pause_time = 2;
+pause_time = length(test)/fs;
 
-tab_t = zeros(1,6);
-tab_av = 1:6;
+tab_t = zeros(1,length(f2));
+tab_av = 1:length(f2);
 
-for i = 1:2
-    rand_v = selectRandom(tab_av)
+test_loops = 10;
+
+for i = 1:test_loops*length(f2)
+    rand_v = selectRandom(tab_av);
     random_f = f2(rand_v);
     tab_t(rand_v) = tab_t(rand_v)+1;
 
-    if 1 == tab_t(rand_v)
-        tab_av = tab_av(tab_av~=rand_v)
+    if test_loops == tab_t(rand_v)
+        tab_av = tab_av(tab_av~=rand_v);
     end
 
-    r_h = 0;
-
     if randi(2) == 2
-        1000
+        1000;
         sound(generate_sine(fs, f1), fs);
         pause(pause_time)
 
-        random_f
+        random_f;
         sound(generate_sine(fs, random_f), fs);
         pause(pause_time)
 
         r_h = 2;
     else
-        random_f
+        random_f;
         sound(generate_sine(fs, random_f), fs);
         pause(pause_time)
 
-        1000
+        1000;
         sound(generate_sine(fs, f1), fs);
         pause(pause_time)
 
@@ -61,21 +61,23 @@ save("correcness_data.mat", "f1", "f2", "correctness", "un_correctness");
 load("correcness_data.mat")
 
 X = f2 - ones(1, length(f2)) .* f1;
+
 Y = correctness.*100./10;
 
 b1 = X'\Y';
 
 x_80 = interp1(b1*X, X, 80);
 
+if isnan(x_80)
+    X2 = [X, X(end) + X(1), X(end)+ 2*X(1)];
+    x_80 = interp1(b1*X2, X2, 80);
+end
+
 figure
 hold on
 scatter(X, Y, 'DisplayName', 'procent poprawnych odp');
-plot(X, b1*X, 'DisplayName', 'regresja liniowa');
-
-if ~isnan(x_80)
-    plot(x_80, b1*x_80, 'O', 'DisplayName', 'punkt 80%');
-end
-
+plot(X2, b1*X2, 'DisplayName', 'regresja liniowa');
+plot(x_80, b1*x_80, 'O', 'DisplayName', strcat('punkt Y=80% X = ', num2str(x_80)));
 xlabel("Różnica częstotliwości [Hz]");
 ylabel("procent poprawnie zgadniętych [%]");
 legend();
@@ -90,7 +92,7 @@ function x = generate_sine(fs, f)
     x(1:fs/100) = x(1:fs/100) .* logspace(0, 1, fs/100);
     x(end - fs/100+1:end) = x(end - fs/100+1:end) .* logspace(1, 0, fs/100);
     
-    x = [x, zeros(1, fs)];
+    x = [x, zeros(1, fs/10)];
 end
 
 
